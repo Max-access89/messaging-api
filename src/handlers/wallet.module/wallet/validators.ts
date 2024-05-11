56478ab769ca2267e8d44a59b437035387f69f9d
Max-access89 <maxwell@access89.com>
2024-05-11 15:07:27 +0000

    clean out forecast by leader

---
+++ src/handlers/wallet.module/wallet/validators.ts
@@ -0,0 +1,16 @@
+// import { z } from "zod";
+
+// export const CreateWalletSchema = z.object({
+//   id: z.string().min(1),
+//   wallet_value: z.number().default(0),
+//   wallet_currency: z.string().default("GHS"),
+// });
+
+// export const GetWalletSchema = z.object({
+//   walletId: z.string().min(1),
+// });
+
+// export const UpdateWalletSchema = z.object({
+//   id: z.string().min(1),
+//   amount: z.number(),
+// });
